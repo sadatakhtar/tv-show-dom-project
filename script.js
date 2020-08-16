b9d93c890019ae5642cbcc5b19a1202eacc98b93
Sadat <sadat0akhtar@gmail.com>
2020-08-16 12:26:58 +0100

    completed all level 100 tasks

--- script.js
+++ script.js
@@ -12,5 +12,26 @@
 //window.onload = setup;
 
 //SELECTORS
-let titleEl = document.getElementById('title');
+const allEpisodes = getAllEpisodes();
+let cardEl = document.querySelector('.card');
+
+//let titleEl = document.getElementById('title');
+//titleEl.textContent = `${allEpisodes[0].name} S0${allEpisodes[0].season}E0${allEpisodes[0].number}`;
+for(let i=0;i< allEpisodes.length; i++){
+    let newTitleTag = document.createElement('h3');
+    newTitleTag.textContent = `${allEpisodes[i].name} S0${allEpisodes[i].season}E0${allEpisodes[i].number}`;
+    cardEl.appendChild(newTitleTag);
+
+    let imgTag = document.createElement('img');
+    imgTag.src = `${allEpisodes[i].image.medium}`;
+    cardEl.appendChild(imgTag);
+
+    let pTag = document.createElement('p');
+    pTag.textContent = `${allEpisodes[i].summary}`;
+    cardEl.appendChild(pTag);
+}
+
+
+
+
 
