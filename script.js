fbb5397532f0587f74fc6558d820583ca9f11956
Sadat <sadat0akhtar@gmail.com>
2020-08-20 00:18:47 +0100

    tweaked the code, still not functioning 100% correctly

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-//You can edit ALL of the code here
-// function setup() {
-//   const allEpisodes = getAllEpisodes();
-//   makePageForEpisodes(allEpisodes);
-// }
-
-// function makePageForEpisodes(episodeList) {
-//   const rootElem = document.getElementById("root");
-//   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-// }
-
-//window.onload = setup;
-
 //SELECTORS
 const allEpisodes = getAllEpisodes();
 let cardEl = document.querySelector('.card');
@@ -19,21 +6,22 @@ let showAllBtn = document.getElementById('showAllBtn');
 let clearBtn = document.getElementById('clearBtn');
 
 
-//let titleEl = document.getElementById('title');
-//titleEl.textContent = `${allEpisodes[0].name} S0${allEpisodes[0].season}E0${allEpisodes[0].number}`;
-        // for(let i=0;i< allEpisodes.length; i++){
-        //     let newTitleTag = document.createElement('h3');
-        //     newTitleTag.textContent = `${allEpisodes[i].name} S0${allEpisodes[i].season}E0${allEpisodes[i].number}`;
-        //     cardEl.appendChild(newTitleTag);
+function displayAll(){
+        for(let i=0;i< allEpisodes.length; i++){
+            let newTitleTag = document.createElement('h3');
+            newTitleTag.textContent = `${allEpisodes[i].name} S0${allEpisodes[i].season}E0${allEpisodes[i].number}`;
+            cardEl.appendChild(newTitleTag);
 
-        //     let imgTag = document.createElement('img');
-        //     imgTag.src = `${allEpisodes[i].image.medium}`;
-        //     cardEl.appendChild(imgTag);
+            let imgTag = document.createElement('img');
+            imgTag.src = `${allEpisodes[i].image.medium}`;
+            cardEl.appendChild(imgTag);
 
-        //     let pTag = document.createElement('p');
-        //     pTag.textContent = `${allEpisodes[i].summary}`;
-        //     cardEl.appendChild(pTag);
-        // }
+            let pTag = document.createElement('p');
+            pTag.textContent = `${allEpisodes[i].summary}`;
+            cardEl.appendChild(pTag);
+        }
+}
+
 //EVENT LISTENERS
 showAllBtn.onclick = () => {
     for(let i=0;i< allEpisodes.length; i++){
@@ -54,10 +42,11 @@ showAllBtn.onclick = () => {
 //RESET THE PAGE
 clearBtn.onclick = () =>{
     location.reload();
+  
 }
 
-
- function searchEpisodes(){
+//USED IN HTML ONKEYUP
+function searchEpisodes(){
      let inputValue = searchBar.value;
     console.log(inputValue);
 
